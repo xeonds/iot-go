382fc6636e1abf1b03747124082557cf0ec50e7a
xeonds <xeonds@stu.xidian.edu.cn>
2024-11-25 21:20:35 +0800

    feat: change client 8266 logic

--- client/esp8266/esp8266.ino
+++ client/esp8266/esp8266.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+//#include <ESP8266mDNS.h>
 #include <EEPROM.h>
 #include <regex>
 #include <ArduinoWebsockets.h>
@@ -12,10 +12,21 @@ using namespace websockets;
 const char *ap_ssid = "ESP8266_Setup";
 const char *ap_password = "password";
 
+// gpio configuration
 const int buttonPin = 0; // GPIO0 接入按键
 const int outputPin = 2; // GPIO2 输出控制电平
 
-// 设备配置
+// button input configuration
+bool buttonState = HIGH;
+bool lastState = HIGH;
+unsigned long pressTime = 0;
+unsigned long releaseTime = 0;
+bool isLongPress = false;
+
+// store pwm value
+static int pwmValue = 0;
+
+// device configuration
 const char *deviceType = "smart_light"; // 设备类别
 char deviceId[13];                      // 设备唯一 ID (MAC 地址后 6 位)
 
@@ -41,8 +52,7 @@ Config config;
 
 WebsocketsClient webSocket;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   EEPROM.begin(sizeof(Config));
 
@@ -50,114 +60,106 @@ void setup()
   pinMode(outputPin, OUTPUT);       // 配置为输出引脚
   analogWrite(outputPin, 0);        // 初始为低电平
 
+  // setup websocket event handler
+  webSocket.onMessage(webSocketMessage);
+  webSocket.onEvent(webSocketEvent);
+
+  server.on("/reset", handleReset);
+
   // 生成设备 ID
   String macAddress = WiFi.macAddress();
   macAddress.replace(":", "");
   snprintf(deviceId, sizeof(deviceId), "%s", macAddress.substring(6).c_str());
 
-  // 尝试连接保存的 WiFi
-  // 如果连接失败，启动 AP 模式配网
+  unsigned long reconnectInterval = 1000; // initial reconnect attempt interval
+  unsigned int retryCount=0;
+  // try to connect to saved WiFi
+  // if no wifi saved in config, start ap mode for config
   EEPROM.get(0, config);
-  if (strlen(config.ssid) > 0)
-  {
+  if (strlen(config.ssid) > 0) {
     WiFi.begin(config.ssid, config.password);
-    int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < 20)
-    {
-      delay(500);
-      Serial.print(".");
-      attempts++;
-    }
-    if (WiFi.status() == WL_CONNECTED)
-    {
+    // reconnect forever until connected to network
+reconnect:
+    retryCount++;
+    //reconnectInterval = min(reconnectInterval * 2, 60000);
+    delay(reconnectInterval);
+    Serial.print(".");
+    if (WiFi.status() == WL_CONNECTED ) {
       Serial.println("\nConnected to saved WiFi");
       Serial.print("IP address: ");
       Serial.println(WiFi.localIP());
-
-      // // Start mDNS responder
-      // if (!MDNS.begin(deviceId))
-      // {
-      //   Serial.println("Error setting up MDNS responder!");
-      //   return;
-      // }
-      // Serial.println("mDNS responder started");
-
-      // mdns:
-      // Check the EEPROM and try to ping the server
       Serial.println("Trying to reach server");
       Serial.println(String(config.serverIp));
       Serial.println(String(config.serverPort));
-
-      if (strlen(config.serverIp) > 0 && strlen(config.serverPort) > 0)
-      {
+      if (strlen(config.serverIp) > 0 && strlen(config.serverPort) > 0) {
         Serial.print("Trying to connect to saved server: ");
-        Serial.print(config.serverIp);
-        Serial.print(":");
-        Serial.println(config.serverPort);
-
-        int pingAttempts = 0;
-        bool serverReachable = false;
-        Serial.println("Server found");
+        Serial.print(config.serverIp); Serial.print(":"); Serial.println(config.serverPort);
         HTTPClient http;
         WiFiClient client;
         http.begin(client, "http://" + String(config.serverIp) + ":" + String(config.serverPort) + "/api/register/" + deviceId);
         http.addHeader("Content-Type", "application/json");
         String payload = "{\"deviceId\":\"" + String(deviceId) + "\", \"deviceType\":\"" + String(deviceType) + "\", \"cmds\":\"action:on->null;action:off->null;action:toggle->null;action:get_status->data.status:int;action:+->null;action:-->null;action.pwm:int->null;action:reset->null;\"}";
         int httpCode = http.POST(payload);
-        if (httpCode != HTTP_CODE_OK)
-        {
+        if (httpCode != HTTP_CODE_OK) {
           Serial.printf("Failed to connect, error: %s\n", http.errorToString(httpCode).c_str());
           http.end();
-        }
-        else
-        {
+        } else {
           Serial.println("Device registered successfully");
-          webSocket.onMessage(webSocketMessage);
-          webSocket.onEvent(webSocketEvent);
           webSocket.connect("ws://" + String(config.serverIp) + ":" + String(config.serverPort) + "/ws/device/" + String(deviceId));
           Serial.println("Server connected");
-          return;
         }
-      }
-      Serial.println("Failed to reach server, reconfigure needed");
-      // Serial.println("Failed to reach server, finding by mDNS");
-      // delay(2000);
-      // find_mdns:
-      //   int n = MDNS.queryService("iot-gateway", "tcp", 8000);
-      //   Serial.print(".");
-      //   if (0==n)
-      //   {
-      //     delay(1000);
-      //     goto find_mdns;
-      //   }
-      //   else
-      //   {
-      //     Serial.println("Server found: " + MDNS.IP(0).toString() + ":" + MDNS.port(0));
-      //     // Save the first found server's info to EEPROM
-      //     String serverIp = MDNS.IP(0).toString();
-      //     String serverPort = String(MDNS.port(0));
-      //     EEPROM.put(sizeof(WiFiCredentials), serverIp);
-      //     EEPROM.put(sizeof(WiFiCredentials) + 16, serverPort);
-      //     EEPROM.commit();
-      //     Serial.println("Server info saved to EEPROM");
-      //     goto mdns;
-      // }
-      // return;
-    }
+      } else { goto config; }
+    } else if (retryCount < 16) { goto reconnect; }
+  } else {
+config:
+    server.on("/configure", handleConfigure);
+    server.on("/", handleRoot);
+    setupAP();
   }
-  server.on("/configure", handleConfigure);
-  server.on("/", handleRoot);
+
   server.begin();
-  setupAP();
+  return;
 }
 
-void loop()
-{
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW && lastState == HIGH) {
+    pressTime = millis();
+    isLongPress = false;
+  } 
+  if (buttonState = LOW) {
+    if (!isLongPress && millis() - pressTime >= 5000) { // press longer than 5s
+      handleReset();
+      isLongPress=true;
+    }
+  }
+  if (buttonState==HIGH && lastState==LOW) {
+    releaseTime = millis();
+    if (!isLongPress && releaseTime - pressTime < 5000) {
+      pwmValue = (pwmValue%256+256)%256;
+      pwmValue = pwmValue - (pwmValue%16);
+      pwmValue = (pwmValue+16)%256;
+      analogWrite(outputPin, pwmValue);
+      if (webSocket.available()) {
+        webSocket.send("data.status:" + String(pwmValue));
+      }
+    }
+  }
+  lastState = buttonState;
+
   server.handleClient();
-  MDNS.update();
-  webSocket.poll();
-  // TODO: long press to reset WiFi settings
-  // TODO: short click to switch on/off
+  // websocket autoreconnect
+  if (webSocket.available()) {
+    webSocket.poll();
+  } else if (strlen(config.serverIp)>0 && strlen(config.serverPort)>0) {
+    // reconnect attempt in every loop
+    Serial.println("Attempting to reconnect to WebSocket server...");
+    if (webSocket.connect("ws://" + String(config.serverIp) + ":" + String(config.serverPort) + "/ws/device/" + String(deviceId))) {
+      Serial.println("reconnect success");
+    }
+  }
+
+  delay(10);
 }
 
 // WiFi init
@@ -170,8 +172,7 @@ void setupAP()
   Serial.println(WiFi.softAPIP());
 }
 
-void handleRoot()
-{
+void handleRoot() {
   String html = "<html><body>";
   html += "<h1>ESP8266 WiFi Setup</h1>";
   html += "<form action='/configure' method='post'>";
@@ -183,8 +184,7 @@ void handleRoot()
   html += "</form></body></html>";
   server.send(200, "text/html", html);
 }
-void handleConfigure()
-{
+void handleConfigure() {
   String newSsid = server.arg("ssid");
   String newPassword = server.arg("password");
   String newServerIp = server.arg("ip");
@@ -202,73 +202,59 @@ void handleConfigure()
   delay(1000);
   ESP.restart();
 }
+void handleReset() {
+  for (int i = 0; i < sizeof(Config) + 16 + 16; i++) {
+    EEPROM.write(i, 0);
+  }
+  EEPROM.commit();
+  server.send(200, "text/plain", "Device reset success. Rebooting...");
+  delay(1000);
+  ESP.restart();
+}
 
-void webSocketMessage(WebsocketsMessage message)
-{
+void webSocketMessage(WebsocketsMessage message) {
   Serial.printf("WebSocket Received Text: %s\n", message.data().c_str());
   String dsl = message.data();
   Message msg;
-  if (!ParseDSL(dsl, msg))
-  {
+  if (!ParseDSL(dsl, msg)) {
     Serial.println("Invalid DSL format");
     return;
   }
 
-  static int pwmValue = 0;
-
-  if (msg.Type == "info")
-  {
-    if (msg.Value == "cmds")
-    {
+  if (msg.Type == "info") {
+    if (msg.Value == "cmds") {
       webSocket.send("data.cmds: \"action:on->null;action:off->null;action:toggle->null;action:get_status->data.status:int;action:+->null;action:-->null;action.pwm:int->null;action:reset->null\"");
     }
   }
-  if (msg.Type == "action")
-  {
-    if (msg.Value == "on")
-    {
+  if (msg.Type == "action") {
+    if (msg.Value == "on") {
       pwmValue = 255;
       analogWrite(outputPin, pwmValue);
       webSocket.send("data.status:" + String(pwmValue));
-    }
-    else if (msg.Value == "off")
-    {
+    } else if (msg.Value == "off") {
       pwmValue = 0;
       analogWrite(outputPin, pwmValue);
       webSocket.send("data.status:" + String(pwmValue));
-    }
-    else if (msg.Value == "toggle")
-    {
+    } else if (msg.Value == "toggle") {
       pwmValue = (pwmValue == 0) ? 255 : 0;
       analogWrite(outputPin, pwmValue);
       webSocket.send("data.status:" + String(pwmValue));
-    }
-    else if (msg.Value == "get_status")
-    {
+    } else if (msg.Value == "get_status") {
       webSocket.send("data.status:" + String(pwmValue));
-    }
-    else if (msg.Value == "+")
-    {
+    } else if (msg.Value == "+") {
       pwmValue = min(pwmValue + 16, 255);
       analogWrite(outputPin, pwmValue);
       webSocket.send("data.status:" + String(pwmValue));
-    }
-    else if (msg.Value == "-")
-    {
+    } else if (msg.Value == "-") {
       pwmValue = max(pwmValue - 16, 0);
       analogWrite(outputPin, pwmValue);
       webSocket.send("data.status:" + String(pwmValue));
-    }
-    else if (msg.Key == "pwm")
-    {
+    } else if (msg.Key == "pwm") {
       pwmValue = msg.Value.toInt();
       analogWrite(outputPin, constrain(pwmValue, 0, 255));
       webSocket.send("data.status:" + String(pwmValue));
-    }
-    else if (msg.Value == "reset")
-    {
-      for (int i = 0; i < sizeof(Config) + 16 + 16; i++)
-      {
+    } else if (msg.Value == "reset") {
+      for (int i = 0; i < sizeof(Config) + 16 + 16; i++) {
         EEPROM.write(i, 0);
       }
       EEPROM.commit();
@@ -279,68 +265,45 @@ void webSocketMessage(WebsocketsMessage message)
 
 void webSocketEvent(WebsocketsEvent event, String data)
 {
-  if (event == WebsocketsEvent::ConnectionOpened)
-  {
+  if (event == WebsocketsEvent::ConnectionOpened) {
     Serial.println("WebSocket Connected");
-  }
-  else if (event == WebsocketsEvent::ConnectionClosed)
-  {
+  } else if (event == WebsocketsEvent::ConnectionClosed) {
     Serial.println("WebSocket Disconnected");
-    while (!webSocket.available())
-    {
-      Serial.println("Attempting to reconnect to WebSocket server...");
-      webSocket.connect("ws://" + String(config.serverIp) + ":" + String(config.serverPort) + "/ws/device/" + String(deviceId));
-      delay(5000); // Wait for 5 seconds before retrying
-    }
-    Serial.println("WebSocket Reconnected");
-  }
-  else if (event == WebsocketsEvent::GotPing)
+  } else if (event == WebsocketsEvent::GotPing) {
     Serial.println("WebSocket Got a Ping!");
-  else if (event == WebsocketsEvent::GotPong)
-  {
+  } else if (event == WebsocketsEvent::GotPong) {
     Serial.println("WebSocket Got a Pong!");
   }
 }
 
-bool ParseDSL(String &dsl, Message &message)
-{
+bool ParseDSL(String &dsl, Message &message) {
   std::regex pattern(R"(^(\w+)(?:\.(\w+))?(?:\[(\d+)\])?(?::(.+))?$)");
   std::smatch matches;
 
   std::string dslStr = dsl.c_str();
 
-  if (!std::regex_match(dslStr, matches, pattern))
-  {
+  if (!std::regex_match(dslStr, matches, pattern)) {
     Serial.println("Invalid DSL format");
     return false;
   }
 
   message.Type = matches[1].str().c_str();
 
-  if (matches[2].matched)
-  {
+  if (matches[2].matched) {
     message.Key = matches[2].str().c_str();
-  }
-  else
-  {
+  } else {
     message.Key = "";
   }
 
-  if (matches[3].matched)
-  {
+  if (matches[3].matched) {
     message.Limit = std::stoi(matches[3].str());
-  }
-  else
-  {
+  } else {
     message.Limit = 0;
   }
 
-  if (matches[4].matched)
-  {
+  if (matches[4].matched) {
     message.Value = matches[4].str().c_str();
-  }
-  else
-  {
+  } else {
     message.Value = "";
   }
 
